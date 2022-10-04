d72b7248a902cdc0817caab346599adbffab5513
S-Nivi <99079695+S-Nivi@users.noreply.github.com>
2022-10-04 22:37:23 +0530

    Update urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 
 ]
-
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 
