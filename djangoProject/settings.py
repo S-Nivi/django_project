14810b12ae13a401db8d56ef47f2614734cb8c39
S-Nivi <99079695+S-Nivi@users.noreply.github.com>
2022-10-04 21:51:44 +0530

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')]
+        'DIRS': [BASE_DIR / 'templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
