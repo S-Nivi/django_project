3faefd7a302a136f1a09fd35e713bace9729dbe0
S-Nivi <99079695+S-Nivi@users.noreply.github.com>
2022-10-04 21:21:26 +0530

    Update urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from nivi import views
 
 from django.views.static import serve
@@ -41,7 +42,7 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 
 ]
-
+urlpatterns=urlpatters+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 
